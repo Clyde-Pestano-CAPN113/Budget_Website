afbf87faf6f5e2f595b9769c37a69cd78fbc0118
FeihongChuSecE <faye.chu.fh@gmail.com>
2024-11-19 14:49:08 -0500

    update

---
+++ Information-webpage/information.js
@@ -0,0 +1,16 @@
+//family
+//onclick: change this picture to another picture
+
+document.getElementById('childcare_and_education').onclick = function() {
+  const currentSrc = this.src;
+  const image1 = 'information_img/education-and-childcare-logo.png';
+  const image2 = 'information_img/Screenshot 2024-11-19 091825.png';
+
+  if(currentSrc.endsWith(image1)) {
+    this.src = image2;
+  }else if(currentSrc.endsWith(image2)) {
+    this.src = image1;
+  }else {
+    alert('error');
+  }
+}
