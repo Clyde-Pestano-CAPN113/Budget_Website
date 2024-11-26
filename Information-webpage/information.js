8115906807872ffbaed2b790d6120f8dfc691732
Clyde-Pestano-CAPN113 <Jalenpestano@icloud.com>
2024-11-26 01:14:13 -0500

    Major changes to the entire website
    overhualled alot of elements that we not coherrient with the design we were going for and added new html files

--- Information-webpage/information.js
+++ Information-webpage/information.js
@@ -3,8 +3,8 @@
 
 document.getElementById('childcare_and_education').onclick = function() {
   const currentSrc = this.src;
-  const image1 = 'information_img/education-and-childcare-logo.png';
-  const image2 = 'information_img/Screenshot 2024-11-19 091825.png';
+  const image1 = '../information_img/education-and-childcare-logo.png';
+  const image2 = '../information_img/Screenshot 2024-11-19 091825.png';
 
   if(currentSrc.endsWith(image1)) {
     this.src = image2;
